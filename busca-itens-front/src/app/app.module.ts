da10bc3c71371c9ca48275ba8230c384d18a2015
Ronaldo Jose Guastalli <rjguastalli@desenvvar10-lnx.dimed.local>
2019-05-23 09:21:14 -0300

    [TELA-INICIAL]: adicionado o componente lista a tela inicial.

--- busca-itens-front/src/app/app.module.ts
+++ busca-itens-front/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { ListaComponent } from './shared/lista/lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TelaInicialComponent
+    TelaInicialComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
