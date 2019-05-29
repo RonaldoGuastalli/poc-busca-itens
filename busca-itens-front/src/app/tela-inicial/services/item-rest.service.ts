6618ef3bbd4ac165606afbe1a93800d735d8ddc4
Ronaldo Jose Guastalli <rjguastalli@desenvvar10-lnx.dimed.local>
2019-05-29 14:29:36 -0300

    [ITEM-SERVICE]
    refatorando item-service e item-rest-service.

--- busca-itens-front/src/app/tela-inicial/services/item-rest.service.ts
+++ busca-itens-front/src/app/tela-inicial/services/item-rest.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { ItemRequestModel } from '../model/item-request.model';
 import { EstoqueRequestModel } from '../model/estoque-request.model';
-import { map } from 'rxjs/operators';
-import { element } from '@angular/core/src/render3';
 import { itemDetalhe } from '../model/item-detalhe.model';
 import { PrecoRequestModel } from '../model/preco-request.model';
 import { ItemDetalheCompletoModel } from '../model/item-detalhe-completo.model';
@@ -12,34 +11,26 @@ import { ItemDetalheCompletoModel } from '../model/item-detalhe-completo.model';
 @Injectable()
 export class ItemRestService {
 
-    private teste: Observable<EstoqueRequestModel>;
+    private urlBase: string = 'http://api-int.grupodimedservices.com.br/tst'
 
     constructor(private http: HttpClient) { }
 
     public getListaItens(nomePesquisaItem: string): Observable<ItemRequestModel[]> {
-        return this.http.get<ItemRequestModel[]>(`http://api-int.grupodimedservices.com.br/tst/item/v3/itens/base/autocomplete?nome=${nomePesquisaItem}&codigoFilial=101&maxResult=200&ordenarRentabilidade=true&ordenarPreco=false`);
+        return this.http.get<ItemRequestModel[]>(`${this.urlBase}/item/v3/itens/base/autocomplete?nome=${nomePesquisaItem}&codigoFilial=101&maxResult=200&ordenarRentabilidade=true&ordenarPreco=false`);
     }
 
     public getEstoqueItemNaLoja(codigoItem: number): Observable<EstoqueRequestModel[]> {
-        return this.http.get<ItemRequestModel[]>(`http://api-int.grupodimedservices.com.br/tst/filial/v1/filiais/101/estoque?itens=${codigoItem}`);
+        return this.http.get<ItemRequestModel[]>(`${this.urlBase}/filial/v1/filiais/101/estoque?itens=${codigoItem}`);
     }
 
     buscaDetalhe(codigoItem: number): Observable<ItemRequestModel> {
-        const url: string = `http://api-int.grupodimedservices.com.br/tst/mostruario/v3/itens/detalhe`;
+        const url: string = `${this.urlBase}/mostruario/v3/itens/detalhe`;
         itemDetalhe.itens[0].codigo = codigoItem;
         return this.http.post<ItemRequestModel>(url, itemDetalhe);
     }
 
     public getPrecos(codigoItem: number): Observable<PrecoRequestModel> {
-        const url: string = `http://api-int.grupodimedservices.com.br/tst/mostruario/v3/itens/precos?item=${codigoItem}&filial=101&perfil=1`;
+        const url: string = `${this.urlBase}/mostruario/v3/itens/precos?item=${codigoItem}&filial=101&perfil=1`;
         return this.http.get<ItemRequestModel>(url);
     }
-
-    detalheCompletoDoItem(codigoItem: number): Observable<ItemDetalheCompletoModel> {
-        const url: string = `http://api-int.grupodimedservices.com.br/tst/mostruario/v3/itens/detalhe`;
-        itemDetalhe.itens[0].codigo = codigoItem;
-        return this.http.post<ItemDetalheCompletoModel>(url, itemDetalhe);
-    }
-
-
 }
--- busca-itens-front/src/app/tela-inicial/services/item.service.ts
+++ busca-itens-front/src/app/tela-inicial/services/item.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { Observable,  Subscription, forkJoin } from 'rxjs';
+import { Observable, Subscription, forkJoin } from 'rxjs';
 import { map, find } from 'rxjs/operators';
 
 
@@ -8,59 +8,55 @@ import { ItemRestService } from './item-rest.service';
 import { ItemResponseModel } from '../model/item-response.model';
 import { ItemRequestMapper } from '../mapper/Item-request.mapper';
 import { ItemModelBuilder } from '../build/item-model.builder';
+import { ItemRequestModel } from '../model/item-request.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
-private listaResponse:ItemResponseModel[];
-private listaDeitens: Array<any> = new Array<any>();
+  private listaDeItens: Array<any>;
 
   constructor(private itemRestService: ItemRestService) { }
 
-  // public getItens(): Observable<ItemResponseModel[]>{  
-  //   return this.itemRestService.getListaItens('dipi')
-  //       .pipe(
-  //         map(e => ItemRequestMapper.mapDaLista(e))
-  //         );
-  // }
-
-  // public getEstoque(): Observable<ItemResponseModel[]>{
-  //   return this.itemRestService.getEstoqueItemNaLoja(484160)
-  //     .pipe(
-  //       map(
-  //         e => ItemRequestMapper.estoqueModel(e)
-  //       )
-  //     )
-  // }
-
-  public getItensDaApi(nomeItem: string){
-    this.listaDeitens = new Array<any>();
+  public getItensDaApi(nomeItem: string) {
     this.itemRestService.getListaItens(nomeItem).subscribe(res => {
       res.map(item => {
-        this.getForkItens(item.codigoItem).subscribe(res => {
-          this.listaDeitens.push(ItemModelBuilder.get()
-            .dadosAutocomplete(item)
-            .dadosEstoque(res[0][0])
-            .precoDoProduto(res[2][0])
-            .eanDoItem(res[1].itens[0])
-            .detalheCompleoDoItem(res[1].itens[0])
-            .build())
-        });
-      })     
+        return this.fazerForkJoin(item)})
     });
   }
 
-  public getForkItens(codigo: number): Observable<any>{    
+  public getForkItens(codigo: number): Observable<any> {
     let estoque = this.itemRestService.getEstoqueItemNaLoja(codigo);
     let detalhe = this.itemRestService.buscaDetalhe(codigo);
     let preco = this.itemRestService.getPrecos(codigo);
-    let detalheFull = this.itemRestService.detalheCompletoDoItem(codigo);
-    return forkJoin([estoque, detalhe, preco, detalheFull]);
+    return forkJoin([estoque, detalhe, preco]);
   }
 
-  public getListaItens(){
-    return this.listaDeitens;
+  public getListaItens() {
+    this.listaDeItens = new Array<any>();
+    return this.listaDeItens;
   }
-  
+
+  public fazerForkJoin(item: ItemRequestModel) {
+    return this.getForkItens(item.codigoItem).subscribe(res => {
+      this.pushDosItensPesquisados(item,  res)      
+    });
+  }
+
+  public pushDosItensPesquisados(item: ItemRequestModel, res: any) {    
+    return this.listaDeItens.push(
+      this.montagemDoBuilder(item, res)
+    );
+  }
+
+  public montagemDoBuilder(item: ItemRequestModel, res: any){
+    return ItemModelBuilder.get()
+    .dadosAutocomplete(item)
+    .dadosEstoque(res[0][0])
+    .precoDoProduto(res[2][0])
+    .eanDoItem(res[1].itens[0])
+    .detalheCompleoDoItem(res[1].itens[0])
+    .build();
+  }
+
 }
